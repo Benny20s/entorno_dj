61ad20beecac234a2da1080f40aaabca96b65712
vanessa <va.ulloa@duocuc.cl>
2023-06-24 21:12:59 -0400

    mas

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import inicio, Poleras, Polerones, Funko, Mas
+from .views import inicio, Poleras, Polerones, Funko
 
 urlpatterns = [
     path('',inicio, name="inicio" ),
     path('poleras', Poleras, name="poleras"), 
     path('polerones', Polerones, name="polerones"),
     path('funko', Funko, name="funko"),
-    path('mas', Mas, name="mas"),
 ]
 
